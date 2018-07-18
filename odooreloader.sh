dd160283c9f898ddba3b29b72761dfec7c0f889e
Mohamed Saif <39435025+moesaif@users.noreply.github.com>
2018-07-18 21:52:26 +0300

    Update odooreloader.sh

--- odooreloader.sh
+++ odooreloader.sh
@@ -1,5 +1,5 @@
 #!bin/bash 
-# Author : Mohamed Saif Eldeen (SemiCode Co.) 
+# Author : Mohamed Saif Eldeen (SemiCode Co.) mohamed@semicode.co
 # 17th July 2018
 # Simple Tool to Reload Odoo Correctly after unexpected machine restart
 # OdooReloader V0.1 GPL2
